1663a1ad7c43329988e500ef12296d5cc4345705
Simon <simonkarapetyan605@gmail.com>
2021-10-12 15:58:17 +0400

    added auth validation

--- resources/views/includes/login&&reg/registrationForm.blade.php
+++ resources/views/includes/login&&reg/registrationForm.blade.php
@@ -5,41 +5,43 @@
                 <form class="reg__form" action="{{ route('registration.process') }}" method="POST">
                     @csrf
                     <div class="login-reg__input_cont">
-                        <input class="login-reg__inp" type="text" name="name" placeholder="Name">
+                        <input class="login-reg__inp" type="text" name="name" placeholder="Name" value="{{ old('name') }}">
                         @error('name')
                             <p class="text__red-500">{{ $message }}</p>
                         @enderror
                     </div>
 
                     <div class="login-reg__input_cont">
-                        <input class="login-reg__inp" type="text" name="lastName" placeholder="Last Name">
+                        <input class="login-reg__inp" type="text" name="lastName" placeholder="Last Name"
+                        value="{{ old('lastName') }}">
                         @error('lastName')
                             <p class="text__red-500">{{ $message }}</p>
                         @enderror
                     </div>
 
                     <div class="login-reg__input_cont">
-                        <input class="login-reg__inp" type="email" name="email" placeholder="Email">
+                        <input class="login-reg__inp" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
                         @error('email')
                             <p class="text__red-500">{{ $message }}</p>
                         @enderror
                     </div>
 
                     <div class="login-reg__input_cont">
-                        <input class="login-reg__inp" type="text" name="login" placeholder="Login">
+                        <input class="login-reg__inp" type="text" name="login" placeholder="Login" value="{{ old('login') }}">
                         @error('login')
                             <p class="text__red-500">{{ $message }}</p>
                         @enderror
                     </div>
 
                     <div class="login-reg__input_cont">
-                        <input class="login-reg__inp" type="password" name="password" placeholder="Password">
+                        <input class="login-reg__inp" type="password" name="password" placeholder="Password"
+                        value="{{ old('password') }}">
                         @error('password')
                             <p class="text__red-500">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="login-reg__input_cont">
-                        <input class="login-reg__inp" type="password" name="password_confirmation"
+                        <input class="login-reg__inp" type="password" name="password_confirmation"  value="{{ old('password_confirmation') }}"
                             placeholder="Confirm Password">
                     </div>
                     <div class="auth__more">
